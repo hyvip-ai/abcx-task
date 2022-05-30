e7146d0b2404cb532069fd608732589e3291e04a
rajatcdb <rajat@codebuddy.co>
2022-05-31 00:05:25 +0530

    Updates

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import reactDom from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 reactDom.render(<App />, document.getElementById('root'));
